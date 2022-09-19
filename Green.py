99e6273280b5427777b4215d0ead6dd20d061d38
MR DARK <105338726+JISAN-404@users.noreply.github.com>
2022-09-19 10:40:26 +0600

    Update Green.py

--- Green.py
+++ Green.py
@@ -1,1 +1,25 @@
-import LR1
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        import LR1
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        import LR2
